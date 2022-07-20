893c49599095f2a2a1f2ef585051af7165c43187
vitosandrin <sandrinvito10@gmail.com>
2022-07-20 15:16:50 -0300

    commit

--- aula1.cpp
+++ aula1.cpp
@@ -4,9 +4,10 @@ using namespace std; //Use IO (cin / cout)
 
 main() //Main function
 {
-	system("chcp 65001") //To use accent in strings
+	system("chcp 65001"); //To use accent in strings
+	//cout<<"\n\n\n"; jump a lot of lines 
 	
 	cout<<"\tHello world"; // \t TAB
 	cout<<"\nWelcome to dev world"; // \n jump line 
-	cout<<"\n\n\n";
+	cout<<"\n Write your age: ";
 }
