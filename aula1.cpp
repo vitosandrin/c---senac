fce943b472c5cd9e491b5f3c08344fa1a2283bd9
vitosandrin <sandrinvito10@gmail.com>
2022-07-20 15:37:57 -0300

    commit

--- aula1.cpp
+++ aula1.cpp
@@ -1,13 +1,21 @@
 //Config require 
-#include<iostream> //Input / output stream - #include<package>
+
+//#include<package> - import 
+#include<iostream> //Input / output stream 
+
 using namespace std; //Use IO (cin / cout)
+int age;
 
 main() //Main function
 {
 	system("chcp 65001"); //To use accent in strings
 	//cout<<"\n\n\n"; jump a lot of lines 
 	
-	cout<<"\tHello world"; // \t TAB
-	cout<<"\nWelcome to dev world"; // \n jump line 
+	cout<<"\t Hello world"; // \t TAB
+	cout<<"\n Welcome to dev world"; // \n jump line 
 	cout<<"\n Write your age: ";
+	cout<<"\n";
+	cin>>age;
+	cout<<"\n Your age is: ";
+	cout<<age;
 }
