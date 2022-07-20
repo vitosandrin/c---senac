04f4c4187fd9d2a8766f01fa58a47d3539baaf72
vitosandrin <sandrinvito10@gmail.com>
2022-07-20 16:34:43 -0300

    calculateaverage

--- aula1.cpp
+++ aula1.cpp
@@ -5,6 +5,17 @@
 
 using namespace std; //Use IO (cin / cout)
 int age;
+float score, score1, score2, score3;
+
+void calculateAverage (float score, float score1, float score2, float score3) {
+	float average = (score+score1+score2+score3)/4;
+	cout<<"\n Your average is: "<<average;
+	if(average >= 7){
+		cout<<"\n You are approved";
+	} else {
+		cout<<"\n You are reproved";
+	};
+}
 
 main() //Main function
 {
@@ -14,7 +25,23 @@ main() //Main function
 	cout<<"\t Hello world"; // \t TAB
 	cout<<"\n Welcome to dev world"; // \n jump line 
 	cout<<"\n Write your age: ";
-	cout<<"\n";
 	cin>>age;
-	cout<<"\n Your age is: "<<age;
+	cout<<"\n Your age is: "<<age<<" years old";
+	
+	if(age >= 18){
+		cout<<"\n You are of legal age";
+	} else {
+		cout<<"\n You are underage";
+	};
+	
+	cout<<"\n Enter test scores: ";
+	cin>>score;
+	cin>>score1;
+	cin>>score2;
+	cin>>score3;
+	cout<<"\n Your scores is: "<<score<<" ,"<<score1<<" ,"<<score2<<" ,"<<score3;
+	
+	calculateAverage(score, score1, score2, score3);
+	
+	
 }
