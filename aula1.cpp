481768566d46b366f9a4e452fa24f3b6278cf6e0
vitosandrin <sandrinvito10@gmail.com>
2022-07-20 16:48:05 -0300

    commit

--- aula1.cpp
+++ aula1.cpp
@@ -9,11 +9,11 @@ float score, score1, score2, score3;
 
 void calculateAverage (float score, float score1, float score2, float score3) {
 	float average = (score+score1+score2+score3)/4;
-	cout<<"\n Your average is: "<<average;
+	cout<<"\n\n Your average is: "<<average;
 	if(average >= 7){
-		cout<<"\n You are approved";
+		cout<<"\n You are approved!";
 	} else {
-		cout<<"\n You are reproved";
+		cout<<"\n You are reproved!";
 	};
 }
 
@@ -23,23 +23,21 @@ main() //Main function
 	//cout<<"\n\n\n"; jump a lot of lines 
 	
 	cout<<"\t Hello world"; // \t TAB
-	cout<<"\n Welcome to dev world"; // \n jump line 
-	cout<<"\n Write your age: ";
+	cout<<"\n\n Welcome to dev world"; // \n jump line 
+	cout<<"\n\n Write your age: ";
 	cin>>age;
 	cout<<"\n Your age is: "<<age<<" years old";
 	
 	if(age >= 18){
-		cout<<"\n You are of legal age";
+		cout<<"\n\n You are of legal age";
 	} else {
 		cout<<"\n You are underage";
 	};
 	
-	cout<<"\n Enter test scores: ";
-	cin>>score;
-	cin>>score1;
-	cin>>score2;
-	cin>>score3;
-	cout<<"\n Your scores is: "<<score<<" ,"<<score1<<" ,"<<score2<<" ,"<<score3;
+	cout<<"\n\n Enter test scores[4]: ";
+	cin>>score>>score1>>score2>>score3;
+	
+	cout<<"\n\n Your scores is: "<<score<<" ,"<<score1<<" ,"<<score2<<" ,"<<score3;
 	
 	calculateAverage(score, score1, score2, score3);
 	
